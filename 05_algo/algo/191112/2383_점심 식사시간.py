5f24411fe8fac73840cd035e0cc3b24d6f986e01
choi sol ji <bssj9307@gmail.com>
2019-11-13 19:33:43 +0900

    191113 | algo

--- 05_algo/algo/191112/2383_점심 식사시간.py
+++ 05_algo/algo/191112/2383_점심 식사시간.py
@@ -2,44 +2,70 @@ import sys
 sys.stdin = open('2383.txt', 'r')
 
 
-def powersetlist(li):
-    r = [[]]
-
-    for l in li:
-        temp = [x+[l] for x in r]
-        r += temp
-
-    return r
-
-
 def lunch(li, k, time):
-    st = []
-    while li:
+    lenP = len(li)
+    st = [[] for _ in range(lenP)]
+    pcnt = lenP
+    scnt = 0
+    i = 0
+    j = 0
+    while pcnt > 0:
         time += 1
-        for t in range(len(li)):
+        while i < lenP:
+            if st[i] and time-st[i][0] == k:
+                pcnt -= 1
+                scnt -= 1
+                i += 1
+            else:
+                break
+
+        for t in range(j, lenP):
             if li[t] > 0:
                 li[t] -= 1
             else:
                 li[t] = -1
 
-        while (0 in li or -1 in li) and len(st) < 3:
-            temp = li.pop(0)
-            if temp == 0:
-                st += [time]
-            elif temp == -1:
-                st = [time-1]
-
-        for _ in st:
-            if time-st[0] >= k:
-                st.pop(0)
-
-    while st:
-        for _ in st:
-            if time-st[0] >= k:
-                st.pop(0)
-        time += 1
+        while j < lenP and scnt < 3:
+            if li[j] == 0 or li[j] == -1:
+                temp = li[j]
+                if temp == 0:
+                    st[j] = [time+1]
+                elif temp == -1:
+                    st[j] = [time]
+                j += 1
+                scnt += 1
+            else:
+                break
 
     return time
+
+# def lunch(li, k, time):
+#     st = []
+#     pcnt = len(li)
+#     while pcnt > 0:
+#         time += 1
+#
+#         while st:
+#             if time - st[0] == k:
+#                 st.pop(0)
+#                 pcnt -= 1
+#             else:
+#                 break
+#
+#         for t in range(len(li)):
+#             if li[t] > 0:
+#                 li[t] -= 1
+#             else:
+#                 li[t] = -1
+#
+#         while li and li[0] == (0 or -1) and len(st) < 3:
+#             temp = li.pop(0)
+#             if temp == 0:
+#                 st += [time + 1]
+#             elif temp == -1:
+#                 st += [time]
+#
+#     return time
 
 
 T = int(input())
@@ -58,7 +84,10 @@ for tc in range(1, T+1):
                 else:
                     S += [(a, b, data[a][b])]
 
-    P_powerset = powersetlist(P)
+    P_powerset = [[]]
+    for y in P:
+        temp = [x + [y] for x in P_powerset]
+        P_powerset += temp
 
     min_time = 9999999
 
@@ -67,8 +96,6 @@ for tc in range(1, T+1):
 
         sr1, sc1, k1 = S[0]
         sr2, sc2, k2 = S[1]
-
-        time = 0
 
         time1 = []
         for pr1, pc1 in pset:
@@ -83,6 +110,7 @@ for tc in range(1, T+1):
         t1 = lunch(time1, k1, 0)
         t2 = lunch(time2, k2, 0)
 
+        time = 0
         if t1 < t2:
             time = t2
         else:
